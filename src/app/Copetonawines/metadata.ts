12c85a957e15e785318b70ea72bc741db7bd0041
aguilarivan <ivanaguilarnardin@gmail.com>
2024-10-02 19:13:03 -0300

    fixes

---
+++ src/app/Copetonawines/metadata.ts
@@ -0,0 +1,19 @@
+export const metadata = {
+    title: 'Copetona aniversario',
+    description: 'Tarjeta de invitacion',
+    openGraph: {
+      title: 'Copetona aniversario',
+      description: 'Tarjeta de invitacion',
+      url: 'https://entradita.com/Copetonawines',
+      images: [
+        {
+          url: 'https://photos.fife.usercontent.google.com/pw/AP1GczMX5hKhGqQFphfBxuu3ON2tqR5QkqcF3Fh7wWhPoUMBsTEQUIJwDAHy=w1440-h648-s-no-gm?authuser=0',
+          width: 5480,
+          height: 2466,
+          alt: 'Copetona imagen'
+        }
+      ],
+      type: 'website',
+    },
+  };
+  
---
+++ src/app/Copetonawines/page.tsx
@@ -0,0 +1,9 @@
+import EventPageExtended from "@/components/event-page-extended";
+
+export default function Copetonawines() {
+  return (
+    <div>
+      <EventPageExtended />
+    </div>
+  );
+}
